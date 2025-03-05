1ca2a4b7562c53dd8cfb4b60db8ff6e6a535456c
user <user@example.com>
2025-03-06 04:33:50 +0530

    Add Notes CRUD API controllers

---
+++ controller/note.controller.js
@@ -0,0 +1,117 @@
+import Note from "../models/note.model.js";
+
+export const addNote = async (req, res, next) => {
+  const { title, content } = req.body;
+
+  const { _id } = req.user;
+
+  if (!title) {
+    return res.status(400).json({ error: true, message: "Title is required" });
+  }
+
+  if (!content) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Content is required" });
+  }
+
+  try {
+    const note = new Note({
+      title,
+      content,
+      userId: _id,
+    });
+
+    await note.save();
+
+    res.status(201).json({
+      error: false,
+      message: "Note added successfully",
+      note,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const editNote = async (req, res, next) => {
+  const note = await Note.findById(req.params.noteId);
+
+  if (!note) {
+    return res.status(404).json({ message: "Note not found", error: true });
+  }
+
+  if (req.user._id !== note.userId) {
+    return res
+      .status(401)
+      .json({ message: "You can only update your own note!", error: true });
+  }
+
+  const { title, content } = req.body;
+
+  if (!title && !content && !tags) {
+    return res
+      .status(400)
+      .json({ error: false, message: "No changes provided" });
+  }
+
+  try {
+    if (title) {
+      note.title = title;
+    }
+
+    if (content) {
+      note.content = content;
+    }
+
+    await note.save();
+
+    res.status(200).json({
+      error: false,
+      message: "Note updated successfully",
+      note,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getAllNotes = async (req, res, next) => {
+  const userId = req.user._id;
+
+  try {
+    const notes = await Note.find({ userId: userId });
+
+    res.status(200).json({
+      error: false,
+      message: "All notes retrived successfully",
+      notes,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteNote = async (req, res, next) => {
+  const noteId = req.params.noteId;
+
+  const note = await Note.findOne({ _id: noteId, userId: req.user._id });
+
+  if (!note) {
+    return res.status(400).json({
+      error: true,
+      message: "Note not Found",
+    });
+  }
+
+  try {
+    await Note.deleteOne({ _id: noteId, userId: req.user._id });
+
+    res.status(200).json({
+      error: false,
+      message: "Note deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
