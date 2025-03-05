6972067750b047a63f7826dd51cdd0ece2f9837d
user <user@example.com>
2025-03-06 04:57:49 +0530

    Add main index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+// import routes
+import authRouter from "./routes/auth.route.js";
+import noteRouter from "./routes/note.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1", noteRouter);
+app.get("/", (_, res) => {
+  res.json({ data: "hello from backend" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// error handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Serer Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
